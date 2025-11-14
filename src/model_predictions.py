ce3d8aad0a80d81692d18df471d83d81f748a2f0
Nick Regas <nregas14@icloud.com>
2025-11-14 13:33:28 -0600

    Working on predictions

--- src/model_predictions.py
+++ src/model_predictions.py
@@ -1,10 +1,34 @@
 import pandas as pd
 import os
 from joblib import load
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
 
 
-def model_predictions(tickers, prediction_horizon = 15 ):
-    predicted_returns = {ticker: [] for ticker in tickers}
+def model_predictions(tickers):
+    models = load_models(tickers)
+    project_root = os.path.abspath(os.path.join(os.getcwd(), ".."))
+    model_dir = os.path.join(project_root, "StockPortfolioOptimizer","data","raw")
+    path = os.path.join(model_dir,"TRAINING_DATA.csv")
+    dataset = pd.read_csv(path)
+    mu_vector = []
+
+    for i in range(0,10):
+        model = models[i]
+        features = [col for col in dataset.columns if tickers[i] in col and "Close" not in col]
+        last_features = dataset[features].iloc[-1:]
+        predicted_price = model.predict(last_features)[0]
+
+        last_price = dataset[f"{tickers[i]}_Close"].iloc[-1]
+        predicted_return = (predicted_price - last_price) / last_price
+        mu_vector.append(predicted_return)
+
+
+    return mu_vector
+
+
+
+
 
 def load_models(tickers):
     project_root = os.path.abspath(os.path.join(os.getcwd(), ".."))
@@ -15,7 +39,12 @@ def load_models(tickers):
         path = f"{model_dir}/{ticker}_model.pkl"
         model = load(path)
         models.append(model)
-    print(len(models))
+
+    return models
+
+
+
 
 tickers = ["AAPL", "MSFT", "NVDA", "AMZN", "JNJ", "JPM", "XOM", "CAT", "PG", "NEE"]
-load_models(tickers)
+mu_vector = model_predictions(tickers)
+print(mu_vector)
