b2820e63849fccfefc5ae9a4db3e37cf9658f1bf
Nick Regas <nregas14@icloud.com>
2025-11-14 20:15:37 -0600

    Fixed models

--- src/train_models.py
+++ src/train_models.py
@@ -5,7 +5,8 @@ import pandas as pd
 import os
 import joblib
 
-def train_models(tickers = ["AAPL", "MSFT", "NVDA", "AMZN", "JNJ", "JPM", "XOM", "CAT", "PG", "NEE"]):
+def train_models(tickers = ["AAPL", "MSFT", "NVDA", "AMZN", "JNJ", "JPM", "XOM", "CAT", "PG", "NEE"], horizon = 20):
+
     #Define the path to the data
     project_root = os.path.abspath(os.path.join(os.getcwd(), ".."))
     data_dir = os.path.join(project_root, "StockPortfolioOptimizer", "data", "raw")
@@ -14,26 +15,35 @@ def train_models(tickers = ["AAPL", "MSFT", "NVDA", "AMZN", "JNJ", "JPM", "XOM",
 
     path = os.path.join(data_dir,"TRAINING_DATA.csv")
     train_data = pd.read_csv(path)
+    
 
     for ticker in tickers:
+        train_data = train_data[train_data[f"{ticker}_Close"] != 0]
         features = [col for col in train_data.columns if ticker in col and "Close" not in col]
         x = train_data[features]
-        y = train_data[f'{ticker}_Close'].shift(-1)
-        x, y = x[:-1], y[:-1]
+        future_price = train_data[f'{ticker}_Close'].shift(-horizon)
+        y = (future_price - train_data[f"{ticker}_Close"]) / train_data[f"{ticker}_Close"]
+        x, y = x[:-horizon], y[:-horizon]
 
         X_train, X_test, y_train, y_test = train_test_split(x,y, test_size = .2, shuffle = False)
         model = RandomForestRegressor(n_estimators = 200, random_state = 42)
         model.fit(X_train, y_train)
 
 
+        # Predictions
         preds = model.predict(X_test)
+        
+        # Compute MSE and RMSE in price units
         mse = mean_squared_error(y_test, preds)
-        print(f"{ticker} Test MSE: {mse:.4f}")
+        rmse = mse ** 0.5
 
-        joblib.dump(model, f"{model_dir}/{ticker}_model.pkl")
-        print(f"{ticker} model trained and saved.")
+        print(f"{ticker} Test MSE: {mse:.6f}, RMSE: {rmse:.6f}")
+
+        # Save model
+        joblib.dump(model, os.path.join(model_dir, f"{ticker}_model.pkl"))
+        print(f"{ticker} model trained and saved.\n")
 
 
 if __name__ == "__main__":
     tickers = ["AAPL","MSFT","NVDA","AMZN","JNJ","JPM","XOM","CAT","PG","NEE"]
-    train_models(tickers)
+    train_models(tickers, 60)
