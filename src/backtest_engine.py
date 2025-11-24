def369a8c565d3106bf8ec4f40f16bacb6991dc2
Nick Regas <nregas14@icloud.com>
2025-11-23 18:50:05 -0600

    Finished the core of backtester

--- src/backtest_engine.py
+++ src/backtest_engine.py
@@ -1,20 +1,42 @@
 # Backtest Engine
+import pandas as pd
+import numpy as np
 
 class BacktestEngine:
 
-    def __init__(self, weights, initial_value, filepath):
-        self.weights = weights
-        self.initial_value = initial_value
-        self.filepath = filepath
+    def __init__(self, weights, initial_value, filepath, tickers):
+        self.weights = np.array(weights)
+        self.data = pd.read_csv(filepath)
+        self.day = 0
+        self.current_value = initial_value
+        self.values = [initial_value]
+        self.tickers = tickers
+
         
 
     def simulate_one_day(self):
-        pass
+        day_data = self.data.iloc[self.day]
+        returns_for_day = []
+
+        for ticker in self.tickers:
+            returns_for_day.append(day_data[f"{ticker}_Close"])
+        
+        weighted_return = np.dot(self.weights, returns_for_day)
+        self.current_value = self.current_value * (1 + weighted_return)
+        self.values.append(self.current_value)
+        return self.current_value
+
+            
+
 
     def run_backtest(self, num_days):
-        ending_price = self.initial_value
+        
         for i in range(num_days):
-            new_price = self.simulate_one_day()
+            if (i < len(self.data)):
+                self.simulate_one_day()
+                self.day += 1
+
+        return self.current_value
 
     def compute_metrics():
         pass
--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 from model_predictions import model_predictions
 from optimized_weights import optimizer
 import numpy as np
+from backtest_engine import BacktestEngine
+import os
 
 def main(tickers, horizon = 20):
+
+
+
     mu_array, cov_matrix = model_predictions(tickers)
 
     print(optimizer(mu_array, cov_matrix))
@@ -23,6 +28,17 @@ def main(tickers, horizon = 20):
     print(f"Expected return (horizon): {exp_return:.6%}")
     print(f"Volatility (horizon): {vol:.6%}")
     print(f"Sharpe (horizon): {sharpe:.3f}")
+
+    project_root = os.path.abspath(os.path.join(os.getcwd(), ".."))
+    data_dir = os.path.join(project_root, "StockPortfolioOptimizer", "data", "raw")
+    path = os.path.join(data_dir,"TRAINING_DATA.csv")
+
+    optimized_backtester = BacktestEngine(w,10000, path, tickers)
+    print(optimized_backtester.run_backtest(100))
+
+    even_weights = np.ones(len(tickers)) / len(tickers)
+    even_backtester = BacktestEngine(even_weights,10000, path, tickers)
+    print(even_backtester.run_backtest(100))
 
 
 
