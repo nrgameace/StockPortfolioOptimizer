ce989511288e4c0564ff2e7d44fe82f81f620903
Nick Regas <nregas14@icloud.com>
2025-11-23 23:55:48 -0600

    Finished the product

--- src/backtest_engine.py
+++ src/backtest_engine.py
@@ -1,19 +1,27 @@
 # Backtest Engine
 import pandas as pd
 import numpy as np
+import os
 
 class BacktestEngine:
 
-    def __init__(self, weights, initial_value, filepath, tickers):
+    # Constructor for Backtest Engine
+    def __init__(self, weights, initial_value, tickers):
+        # Define dataset path
+        project_root = os.path.abspath(os.path.join(os.getcwd(), ".."))
+        data_dir = os.path.join(project_root, "StockPortfolioOptimizer", "data", "raw")
+        path = os.path.join(data_dir,"TRAINING_DATA.csv")
+
+        # Initialize instance variables
         self.weights = np.array(weights)
-        self.data = pd.read_csv(filepath)
+        self.data = pd.read_csv(path)
         self.day = 0
         self.current_value = initial_value
         self.values = [initial_value]
         self.tickers = tickers
 
         
-
+    # Simulates the return for one day and calculates the new portfolio value 
     def simulate_one_day(self):
         day_data = self.data.iloc[self.day]
         returns_for_day = []
@@ -24,13 +32,10 @@ class BacktestEngine:
         weighted_return = np.dot(self.weights, returns_for_day)
         self.current_value = self.current_value * (1 + weighted_return)
         self.values.append(self.current_value)
-        return self.current_value
-
-            
 
 
+    # Simulates the given weights over a specified amount of time
     def run_backtest(self, num_days):
-        
         for i in range(num_days):
             if (i < len(self.data)):
                 self.simulate_one_day()
@@ -38,5 +43,22 @@ class BacktestEngine:
 
         return self.current_value
 
-    def compute_metrics():
-        pass
+    # Computes several metrics based on backtesting results
+    def compute_metrics(self):
+        total_return = self.current_value / self.values[0] - 1
+
+        daily_returns = np.diff(self.values) / self.values[:-1]
+
+        num_days = len(self.values) - 1
+        cagr = (self.current_value / self.values[0])**(252 / num_days) - 1
+
+        daily_volatility = np.std(daily_returns)
+
+        sharpe_ratio = (np.mean(daily_returns) / daily_volatility) * np.sqrt(252)
+
+        return {
+        "Total Return": total_return,
+        "CAGR": cagr,
+        "Daily Volatility": daily_volatility,
+        "Sharpe Ratio": sharpe_ratio
+        }
--- src/main.py
+++ src/main.py
@@ -2,23 +2,26 @@ from model_predictions import model_predictions
 from optimized_weights import optimizer
 import numpy as np
 from backtest_engine import BacktestEngine
-import os
+from train_models import train_models
+from download_stock_data import download_stocks
 
-def main(tickers, horizon = 20):
+def main(tickers, horizon = 20, new_stocks = False):
 
-
+    if new_stocks:
+        download_stocks(tickers)
+        train_models(tickers, horizon)
 
     mu_array, cov_matrix = model_predictions(tickers)
 
     print(optimizer(mu_array, cov_matrix))
 
-    eps = 1e-8
-    w = np.maximum(mu_array, eps)        
-    w = w / w.sum()                   # renormalize to sum to 1
+    w = optimizer(mu_array, cov_matrix)  # get optimized weights               
 
     # Metrics
-    exp_return = float(mu_array @ w)        # expected portfolio return 
-    vol = float(np.sqrt(w @ cov_matrix @ w))  # portfolio std dev
+    # expected portfolio return 
+    exp_return = float(mu_array @ w)        
+    # portfolio std dev
+    vol = float(np.sqrt(w @ cov_matrix @ w))  
     sharpe = exp_return / vol if vol > 0 else np.nan
 
     # Print Metrics
@@ -29,20 +32,18 @@ def main(tickers, horizon = 20):
     print(f"Volatility (horizon): {vol:.6%}")
     print(f"Sharpe (horizon): {sharpe:.3f}")
 
-    project_root = os.path.abspath(os.path.join(os.getcwd(), ".."))
-    data_dir = os.path.join(project_root, "StockPortfolioOptimizer", "data", "raw")
-    path = os.path.join(data_dir,"TRAINING_DATA.csv")
-
-    optimized_backtester = BacktestEngine(w,10000, path, tickers)
-    print(optimized_backtester.run_backtest(100))
+    # Backtesting
+    optimized_backtester = BacktestEngine(w,10000, tickers)
+    print(optimized_backtester.run_backtest(60))
 
     even_weights = np.ones(len(tickers)) / len(tickers)
-    even_backtester = BacktestEngine(even_weights,10000, path, tickers)
-    print(even_backtester.run_backtest(100))
+    even_backtester = BacktestEngine(even_weights,10000, tickers)
+    print(even_backtester.run_backtest(60))
 
 
 
 
 if __name__ == "__main__":
     tickers = ["AAPL","MSFT","NVDA","AMZN","JNJ","JPM","XOM","CAT","PG","NEE"]
-    main(tickers, 20)
+    new_stocks = False
+    main(tickers, 20, new_stocks)
